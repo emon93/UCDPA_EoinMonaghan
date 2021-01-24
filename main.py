96f7cff2b5d8b4f5c600754687fda0de4a5f9eab
eoinmonaghan <emonaghan93@gmail.com>
2021-01-24 19:50:47 +0000

    2nd Commit

--- main.py
+++ main.py
@@ -17,12 +17,26 @@ property_prices['Year'] = pd.DatetimeIndex(property_prices['Date of Sale (dd/mm/
 print(property_prices[['Year', 'Price (€)']].head(5))
 # Show year and price column (list included above)
 
-property_prices = property_prices.set_index('Year')
-# Set Index to year on property prices register - Indexing
+property_prices = property_prices.set_index(['Year','County'])
+# Set indexes to year and county on property prices register - Indexing
 
 print(property_prices.head(-5))
 # Property prices last 5 lines printed
 
-avg_property_prices = property_prices.groupby(['Year', 'County'])['Price (€)'].mean()
-print(avg_property_prices)
-# Average house prices grouped by county and sorted by ascending and descending order
+property_prices['Price Value'] = (property_prices['Price (€)'].replace( '[\€,)]','', regex=True )
+               .replace( '[(]','-',   regex=True ).astype(float))
+property_prices['Price Value'].round(decimals=1)
+print(property_prices['Price Value'].head(5))
+# Add column which includes converted Currency column to float and round figures
+
+avg_property_prices = property_prices.groupby('County', as_index=True)['Price Value'].mean()
+# Average house prices grouped by county between 2010 and 2019
+
+avg_dif_price = avg_property_prices.loc['Dublin'] - avg_property_prices.loc['Westmeath']
+avg_dif_price.astype(int)
+print(avg_dif_price.round(0))
+# Comparing Dublin and Westmeath counties average house prices over the last 10 years (using loc)
+# Dublin houses are on average c.241k more expensive
+
+
+
