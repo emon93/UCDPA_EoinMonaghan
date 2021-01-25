1e61118629d7f92d33cca294cec4bcba50b79381
eoinmonaghan <emonaghan93@gmail.com>
2021-01-25 23:48:14 +0000

    3rd Commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ property_prices['Year'] = pd.DatetimeIndex(property_prices['Date of Sale (dd/mm/
 print(property_prices[['Year', 'Price (€)']].head(5))
 # Show year and price column (list included above)
 
-property_prices = property_prices.set_index(['Year','County'])
+property_prices = property_prices.set_index(['Year'])
 # Set indexes to year and county on property prices register - Indexing
 
 print(property_prices.head(-5))
@@ -30,13 +30,25 @@ print(property_prices['Price Value'].head(5))
 # Add column which includes converted Currency column to float and round figures
 
 avg_property_prices = property_prices.groupby('County', as_index=True)['Price Value'].mean()
-# Average house prices grouped by county between 2010 and 2019
+highest_avg_price = avg_property_prices.sort_values(ascending=False).round(0)
+print(highest_avg_price)
+# Average house prices grouped by county between 2010 and 2019 and sorted in descending order in terms of average prices
+# Dublin and the commuter counties are at the top of the list showing the demand to be close to the city
 
-avg_dif_price = avg_property_prices.loc['Dublin'] - avg_property_prices.loc['Westmeath']
-avg_dif_price.astype(int)
-print(avg_dif_price.round(0))
+price_dif1 = avg_property_prices.loc['Dublin'] - avg_property_prices.loc['Westmeath']
+price_dif1.astype(int)
+print(price_dif1.round(0))
 # Comparing Dublin and Westmeath counties average house prices over the last 10 years (using loc)
-# Dublin houses are on average c.241k more expensive
+# Dublin houses are on average c.241k more expensive than Athlone houses
+
+print(highest_avg_price.iloc[0] - highest_avg_price.iloc[-1])
+# Comparing highest and lowest average house prices over the last 10 years (using iloc)
+# The highest is Dublin abd lowest is Longford as noted from the sorted list previously
+
+
+
+# Filtering for full time workers average earnings
+
 
 
 
