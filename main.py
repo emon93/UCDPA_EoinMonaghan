2108d5aab6307ff28c89b8a6b5e34c7b58063b88
eoinmonaghan <emonaghan93@gmail.com>
2021-01-26 23:31:47 +0000

    4th Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 property_prices = pd.read_csv('/Users/eoinmonaghan/PycharmProjects/UCDRubricProject/Property_Price_Register_Ireland-05-04-2019 2.csv')
 # Import a CSV file into a Pandas DataFrame
 
-annual_earnings = pd.read_csv('/Users/eoinmonaghan/PycharmProjects/UCDRubricProject/Average Total Earnings .csv')
+annual_earnings = pd.read_csv('/Users/eoinmonaghan/PycharmProjects/UCDRubricProject/Average Total Earnings by Sector.csv')
 # Import a second CSV file into a Pandas DataFrame
 
 print(property_prices.head(5))
@@ -37,17 +37,36 @@ print(highest_avg_price)
 
 price_dif1 = avg_property_prices.loc['Dublin'] - avg_property_prices.loc['Westmeath']
 price_dif1.astype(int)
-print(price_dif1.round(0))
+print('The difference between Dublin and Westmeath average house prices is', price_dif1.round(0), 'euros.')
 # Comparing Dublin and Westmeath counties average house prices over the last 10 years (using loc)
-# Dublin houses are on average c.241k more expensive than Athlone houses
+# Dublin houses are on average c.241k more expensive than Westmeath houses
 
-print(highest_avg_price.iloc[0] - highest_avg_price.iloc[-1])
+print('The difference between the highest average priced county and the lowest is' , highest_avg_price.iloc[0] - highest_avg_price.iloc[-1], 'euros.')
 # Comparing highest and lowest average house prices over the last 10 years (using iloc)
-# The highest is Dublin abd lowest is Longford as noted from the sorted list previously
+# The highest is Dublin and lowest is Longford as noted from the sorted list previously
 
-
-
+full_time_annual_earnings = annual_earnings[annual_earnings['Type of Employment'].str.contains("Full-time")]
 # Filtering for full time workers average earnings
+
+full_time_annual_earnings1 = full_time_annual_earnings.set_index(['Year', 'NACE Rev 2 Economic Sector'])
+print(full_time_annual_earnings1)
+# Setting economic sector as the index of the dataframe
+
+avg_earnings_per_sector = full_time_annual_earnings1.groupby('NACE Rev 2 Economic Sector', as_index=True)['VALUE'].mean()
+highest_avg_earnings_per_sector = avg_earnings_per_sector.sort_values(ascending=False)
+print(highest_avg_earnings_per_sector.head(10).round(0))
+# Getting the average earnings for full time workers per economic sector and arranging them in ascending order
+# Information and communication is nearly top of the list which is interesting.
+
+import numpy as np
+avg_earnings_per_sector_np = np.array(highest_avg_earnings_per_sector)
+value_np = avg_earnings_per_sector_np[:-1]
+print('The max full time average earnings during the period was', value_np.max().round(0), 'euros')
+print('The min full time average earnings during the period was',value_np.min().round(0), 'euros')
+
+
+
+
 
 
 
